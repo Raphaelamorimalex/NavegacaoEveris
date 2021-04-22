9280d4c902115e6377ab109cdc18a98773e3c28a
Raphaelamorimalex <67813638+Raphaelamorimalex@users.noreply.github.com>
2021-04-21 21:49:52 -0300

    Primeira parte desenvolvida

--- app/src/main/java/com/example/navegacaoeveris/ContactAdapter.kt
+++ app/src/main/java/com/example/navegacaoeveris/ContactAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.navegacaoeveris
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,13 +12,20 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     private val list: MutableList<Contact> = mutableListOf()
 
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val tvname: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvtelephone: TextView = itemView.findViewById(R.id.tv_telephone)
+        private val tvphoto: ImageView = itemView.findViewById(R.id.tv_photo)
         fun bind(contact: Contact){
+            tvname.text = contact.name
+            tvtelephone.text = contact.telephone
+
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view =  LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
@@ -24,5 +34,11 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
 
     override fun getItemCount(): Int {
         return list.size
+    }
+    fun upDateList(list: List<Contact>){
+            this.list.clear()
+            this.list.addAll(list)
+            notifyDataSetChanged()
+
     }
 }
--- app/src/main/java/com/example/navegacaoeveris/MainActivity.kt
+++ app/src/main/java/com/example/navegacaoeveris/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.navegacaoeveris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        upDateList()
+    }
+
+    private fun upDateList(){
+        adapter.upDateList(
+            arrayListOf(
+                Contact(
+                    name = "Raphael Amorim",
+                    telephone = "(32)98477-7965",
+                    photo = "img.png"
+                )
+            )
+        )
     }
 }
